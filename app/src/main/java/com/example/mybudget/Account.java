89992d0b4116f19ae0ba375329e7dca96d5fc4e3
AlinTAT-GitHub <alintamas26@gmail.com>
2023-08-28 10:57:23 +0300

    part commit

---
+++ app/src/main/java/com/example/mybudget/Account.java
@@ -0,0 +1,14 @@
+package com.example.mybudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account);
+    }
+}
---
+++ app/src/main/java/com/example/mybudget/Statistics.java
@@ -0,0 +1,14 @@
+package com.example.mybudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Statistics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statistici);
+    }
+}
